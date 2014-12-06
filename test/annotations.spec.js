9a0f8dd3765c1caf285c47a69d71a4d9fbed391b
Rob Eisenberg <rob@bluespire.com>
2014-12-06 01:59:10 -0500

    chore(*): set up the project structure and build
    This commit adds the initial source code, sets up the project
    structure, creates the gulp build file and configures karma.

---
+++ test/annotations.spec.js
@@ -0,0 +1,48 @@
+import {
+  getAnnotation, 
+  getAllAnnotations, 
+  addAnnotation,
+  normalize
+} from '~/index';
+
+describe('annotations', () => {
+  it('can be located by type', () => {
+    var found = getAnnotation(HasAnnotations, SampleAnnotation);
+    expect(found instanceof SampleAnnotation).toBe(true);
+  });
+
+  it('can be located by base type', () => {
+    var found = getAllAnnotations(HasAnnotations, BaseAnnotation);
+    expect(found.length).toBe(2);
+  });
+
+  it('can be normalized to handle the fallback metadata location', () => {
+    normalize(HasFallbackAnnotations);
+    var found = getAnnotation(HasFallbackAnnotations, SampleAnnotation);
+    expect(found instanceof SampleAnnotation).toBe(true);
+  });
+
+  it('can be dynamically added', () => {
+    var found = getAllAnnotations(HasOneAnnotation, SampleAnnotation);
+    expect(found.length).toBe(1);
+
+    addAnnotation(HasOneAnnotation, new SampleAnnotation());
+    found = getAllAnnotations(HasOneAnnotation, SampleAnnotation);
+    expect(found.length).toBe(2);
+  });
+
+  class BaseAnnotation{}
+  class SampleAnnotation extends BaseAnnotation {}
+
+  class HasAnnotations{}
+  HasAnnotations.annotations = [new SampleAnnotation(), new SampleAnnotation()];
+
+  class HasFallbackAnnotations{
+    static annotations(){
+      return [new SampleAnnotation()];
+    }
+  }
+
+  class HasOneAnnotation{}
+  HasOneAnnotation.annotations = [new SampleAnnotation()];
+});
