5e5e1145182f21f666842e4923e68a65a228f63f
Rob Eisenberg <rob@bluespire.com>
2015-01-07 21:04:08 -0500

    test(Origin): add tests for static property varients
    This commit also rewords several tests and moves class definitions into
    function scopes in order to prevent conflicts.

--- test/origin.spec.js
+++ test/origin.spec.js
@@ -5,36 +5,67 @@ describe('origin', () => {
       origin2 = new Origin('ModuleId2', 'ModuleMember2');
 
   describe('get', () => {
-    it('should return null if fn.__origin__ is unset', () => {
+    it('should return null if not set', () => {
+      class HasNoOrigin {}
       expect(Origin.get(HasNoOrigin)).toBe(null);
     });
 
-    it('should return the origin if fn.__origin__ is set', () => {
+    it('should return the origin if set', () => {
+      class HasOrigin {}
+      Origin.set(HasOrigin, origin1);
+
       expect(Origin.get(HasOrigin)).toBe(origin1);
     });
 
-    it('should prefer fn.origin if it exists', () => {
+    it('should normalize static origin property with Origin value', () => {
+      class HasNoOrigin {}
       HasNoOrigin.origin = origin2;
-      expect(Origin.get(HasNoOrigin)).toBe(origin2);
 
-      // Check to see if it's now set.
-      expect(HasNoOrigin.__origin__).toBe(origin2);
+      expect(Origin.get(HasNoOrigin)).toBe(origin2);
+      expect(HasNoOrigin.origin).toBe(origin2);
+    });
+
+    it('should normalize static origin property with string value', () => {
+      class HasNoOrigin {}
+      HasNoOrigin.origin = 'TestOrigin';
+
+      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
+      expect(HasNoOrigin.origin).toBe('TestOrigin');
+    });
+
+    it('should normalize static origin property with function():string value', () => {
+      class HasNoOrigin {
+        static origin(){ return 'TestOrigin' }
+      }
+
+      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
+      expect(typeof HasNoOrigin.origin === 'function').toBe(true);
+    });
+
+    it('should normalize static origin property with function():Origin value', () => {
+      class HasNoOrigin {
+        static origin(){ return new Origin('TestOrigin'); }
+      }
+
+      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
+      expect(typeof HasNoOrigin.origin === 'function').toBe(true);
     });
   });
 
   describe('set', () => {
     it('should attach an origin if one does not exist', () => {
+      class HasNoOrigin {}
       Origin.set(HasNoOrigin, origin2);
-      expect(HasNoOrigin.__origin__).toBe(origin2);
+
+      expect(Origin.get(HasNoOrigin)).toBe(origin2);
     });
 
     it('should not attach an origin if one exists', () => {
-      Origin.set(HasOrigin, origin2)
-      expect(HasOrigin.__origin__).toBe(origin1);
+      class HasOrigin {}
+      Origin.set(HasOrigin, origin1);
+      Origin.set(HasOrigin, origin2);
+
+      expect(Origin.get(HasOrigin)).toBe(origin1);
     });
   });
-
-  class HasNoOrigin {}
-  class HasOrigin {}
-  HasOrigin.__origin__ = origin1;
 });
