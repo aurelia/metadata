4abfc2469da4db3d3a077cb733c75e0c364f7068
Rob Eisenberg <rob@bluespire.com>
2015-05-13 20:37:07 -0400

    feat(origin): search module registry for module id if not found
    Fixes https://github.com/aurelia/templating-resources/issues/17

--- test/origin.spec.js
+++ test/origin.spec.js
@@ -16,40 +16,6 @@ describe('origin', () => {
 
       expect(Origin.get(HasOrigin)).toBe(origin1);
     });
-
-    it('should normalize static origin property with Origin value', () => {
-      class HasNoOrigin {}
-      HasNoOrigin.origin = origin2;
-
-      expect(Origin.get(HasNoOrigin)).toBe(origin2);
-      expect(HasNoOrigin.origin).toBe(origin2);
-    });
-
-    it('should normalize static origin property with string value', () => {
-      class HasNoOrigin {}
-      HasNoOrigin.origin = 'TestOrigin';
-
-      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
-      expect(HasNoOrigin.origin).toBe('TestOrigin');
-    });
-
-    it('should normalize static origin property with function():string value', () => {
-      class HasNoOrigin {
-        static origin(){ return 'TestOrigin' }
-      }
-
-      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
-      expect(typeof HasNoOrigin.origin === 'function').toBe(true);
-    });
-
-    it('should normalize static origin property with function():Origin value', () => {
-      class HasNoOrigin {
-        static origin(){ return new Origin('TestOrigin'); }
-      }
-
-      expect(Origin.get(HasNoOrigin).moduleId).toBe('TestOrigin');
-      expect(typeof HasNoOrigin.origin === 'function').toBe(true);
-    });
   });
 
   describe('set', () => {
@@ -58,14 +24,6 @@ describe('origin', () => {
       Origin.set(HasNoOrigin, origin2);
 
       expect(Origin.get(HasNoOrigin)).toBe(origin2);
-    });
-
-    it('should not attach an origin if one exists', () => {
-      class HasOrigin {}
-      Origin.set(HasOrigin, origin1);
-      Origin.set(HasOrigin, origin2);
-
-      expect(Origin.get(HasOrigin)).toBe(origin1);
     });
   });
 });
