7c4b75f6cc69640d70a1861eef5eccff483207b0
Binh Vo <bigopon.777@gmail.com>
2021-01-19 22:38:42 +1100

    chore(build): add api doc step back

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ const gulp = require('gulp');
 const bump = require('gulp-bump');
 const conventionalChangelog = require('gulp-conventional-changelog');
 const fs = require('fs');
+const path = require('path');
 const exec = require('child_process').exec;
 const promisify = require('util').promisify;
 const args = require('./build/args');
 const DIR_DOC = './doc';
-const FILE_DOC = DIR_DOC + '/CHANGELOG.md';
+const FILE_DOC = path.resolve(DIR_DOC, 'CHANGELOG.md');
 
 gulp.task('build', async () => {
   const { stderr: err1, stdout: out1 } = await promisify(exec)('npm run build');
@@ -27,6 +28,22 @@ gulp.task('lint', async () => {
   }
   console.log('==== lint output ====');
   console.log(out2);
+});
+
+gulp.task('doc', async () => {
+  const { stderr, stdout } = await promisify(exec)('npm run doc');
+  if (stderr) {
+    console.log('==== doc warning ====');
+    console.log(stderr);
+  }
+  // quite a pain (no API from typeodc) to generate a json string
+  // in memory, so just double write
+  try {
+    const content = fs.readFileSync(DIR_DOC + '/api.json', { encoding: 'utf-8' });
+    fs.writeFileSync(path.resolve(DIR_DOC, 'api.json'), JSON.stringify(JSON.parse(content)));
+  } finally {}
+  console.log('==== doc output ====');
+  console.log(stdout);
 });
 
 gulp.task('changelog', function () {
@@ -49,32 +66,10 @@ gulp.task(
   gulp.series(
     'build',
     'lint',
-    gulp.parallel('bump-version', 'changelog')
+    gulp.parallel(
+      'doc',
+      'bump-version',
+      'changelog'
+    )
   )
 );
-
-// gulp.task('prepare-release', function (cb) {
-//   // const { stderr: err1, stdout: out1 } = await promisify(exec)('npm run build');
-//   // if (err1) {
-//   //   console.log('==== build warning ====');
-//   //   console.log(err1);
-//   // }
-//   // console.log('==== build output ====');
-//   // console.log(out1);
-
-//   // const { stderr: err2, stdout: out2 } = await promisify(exec)('npm run lint');
-//   // if (err2) {
-//   //   console.log('==== lint warning ====');
-//   //   console.log(err2);
-//   // }
-//   // console.log('==== lint output ====');
-//   // console.log(out2)
-//   gulp.series(
-//     'build',
-//     'lint',
-//     // 'bump-version',
-//     // 'doc',
-//     'changelog',
-//     cb
-//   );
-// });
